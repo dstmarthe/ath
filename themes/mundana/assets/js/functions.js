f94a763c65e4e4a6ea8657bede15d628e7e7ea1c
Dale <72896959+dstmarthe@users.noreply.github.com>
2023-02-21 03:46:11 -0500

    added scroll function

--- themes/mundana/assets/js/functions.js
+++ themes/mundana/assets/js/functions.js
@@ -1,7 +1,23 @@
-jQuery( document ).ready(function() {
+jQuery(document).ready(function () {
+	$(window).scroll(function () {
+		$(".topnav").toggleClass("scrollednav py-0", $(this).scrollTop() > 50);
+	});
+});
 
-    $(window).scroll(function(){
-    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
-    });
-    
+window.addEventListener("scroll", function () {
+	var sidebar = document.querySelector(".sidebar");
+	var sidebarHeight = sidebar.offsetHeight;
+	var windowHeight = window.innerHeight;
+	var sidebarTop = sidebar.getBoundingClientRect().top;
+
+	if (sidebarTop >= 0 && sidebarTop + sidebarHeight <= windowHeight) {
+		sidebar.style.position = "fixed";
+		sidebar.style.top = "0";
+	} else if (sidebarTop < 0) {
+		sidebar.style.position = "absolute";
+		sidebar.style.top = "0";
+	} else {
+		sidebar.style.position = "absolute";
+		sidebar.style.top = windowHeight - sidebarHeight + "px";
+	}
 });

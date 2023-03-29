7d43a02619ae935998c251e9752f69cbce0e7f45
Dale <72896959+dstmarthe@users.noreply.github.com>
2023-03-28 22:42:33 -0400

    add contentful preview code

--- themes/mundana/assets/js/functions.js
+++ themes/mundana/assets/js/functions.js
@@ -197,6 +197,12 @@ function render(templateString, data) {
 	return templateString;
 }
 
+// Contentful preview
+const baseUrl =
+	process.env.IS_PREVIEW === "true"
+		? "preview.contentful.com"
+		: "cdn.contentful.com";
+
 // Helper Functions
 function show(elem) {
 	elem.style.display = "block";
